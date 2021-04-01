fd772637cd02a0696f385815e9b5a82d9ca08b62
Izarothy <izarothmail@gmail.com>
2021-04-01 16:57:39 +0200

    Nightblue style

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,14 +8,20 @@ import "./styles/Pytanie.css";
 import "./styles/Appc.css";
 import "./index.css";
 
+const myFunction = () => {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName("li");
+};
 const Strona = () => {
   return (
     <div>
       <iframe
         id="dsc"
         src="https://discord.com/widget?id=484645229371064332&theme=dark"
-        width="350"
-        height="500"
         allowtransparency="true"
         frameborder="0"
         sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
--- src/pages/components/Obramowka.js
+++ src/pages/components/Obramowka.js
@@ -7,17 +7,43 @@ function Obramowka() {
   return (
     <div className="obramowka">
       <div>
-        <Search />
+        {/* <Search /> */}
         <Pytanie
           id="1"
           tekst="W której miejscowości Śródziemia jest karczma Pod Rozbrykanym Kucykiem?"
           odpowiedz="Bree"
         />
-        <Pytanie id="2" tekst={template} />
-        <Pytanie id="3" tekst={template} />
-        <Pytanie id="4" tekst={template} />
-        <Pytanie id="5" tekst={template} />
-        <Pytanie id="6" tekst={template} />
+        <Pytanie
+          id="2"
+          tekst="Kim okazał się być Łazik/Obieżyświat?"
+          odpowiedz="Aragornem"
+        />
+        <Pytanie
+          id="3"
+          tekst="Kogo z kompani Thorina w Rivendell spotkał Frodo?"
+          odpowiedz="Gloina"
+        />
+        <Pytanie
+          id="4"
+          tekst="Ilu członków liczyła drużyna pierścienia?"
+          odpowiedz="Dziewięciu"
+        />
+        <Pytanie id="5" tekst="Kto napisał Księgę Mazarbul?" odpowiedz="Ori" />
+        <Pytanie
+          id="6"
+          tekst="Kto pomógł hobbitom po tym jak napadło ich drzewo?"
+          odpowiedz="Tom Bombadil"
+        />
+        <Pytanie
+          id="7"
+          tekst="Kim jest Tom Bombadil?"
+          odpowiedz="Nie wiadomo"
+        />
+        <Pytanie
+          id="8"
+          tekst="Z Kim walczył Gandalf na szczycie góry?"
+          odpowiedz="Z Balrogiem"
+        />
       </div>
     </div>
   );
